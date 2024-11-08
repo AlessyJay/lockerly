5dbef65db9278981c4b39f713de4c9b6880fe3c8
Tony Peeranat <TPeeranat@micamp.com>
2024-11-08 15:01:16 -0700

    Implement layout for auth page

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Lockerly",
+    template: "%s | Lockerly",
+  },
+  description:
+    "Lockerly - A website that allows users to store and manage their files in a secure and organized manner. It provides a range of features, including file upload and download, version control, collaboration tools, and access control. Lockerly aims to simplify the process of storing and sharing files, making it easy to access and manage files from anywhere.",
+  icons: {
+    icon: "/images/icon1_trans.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl">Lockerly - The only storage you ever need</h1>
+    </div>
+  );
+}
